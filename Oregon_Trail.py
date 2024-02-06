7e6ab49d77680019017fb630f7bc37512f4638df
Maykr1 <eclark72713@gmail.com>
2024-02-05 21:16:21 -0500

    Created a StartGame function and did some general shaping

--- Oregon_Trail.py
+++ Oregon_Trail.py
@@ -1,3 +1,5 @@
+import random
+
 class Character:
     def __init__(self, name, age, health):
         self.name = name
@@ -10,11 +12,10 @@ class Character:
     def loseHealth(self, health):
         self.health -= health
 
-def createCharacterMenu():
-    name = input("What is the name of your first character?")
-    char1 = Character(name, 40, 100)
-    name = input("What is the name of your second character?")
-    char2 = Character(name, 35, 100)
+def startGame():
+    print("You set out on March 20th, 1857 on the Oregon Trail. Your destination is Oregon City. Your characters {} and {} must survive the grueling path.".format(char1.name, char2.name))
+    while char1.name > 0 and char2.name > 0:
+        pass
 
 if __name__ == '__main__':
     menu = input("""Hello and Welcome to the Oregon Trail Game!
@@ -23,10 +24,14 @@ if __name__ == '__main__':
     1. Start Game
     2. Quit Game
         
-        """)
+    """)
     
     if menu == '1':
-        createCharacterMenu()
+        name = input("What is the name of your first character? ")
+        char1 = Character(name, 40, 100)
+        name = input("What is the name of your second character? ")
+        char2 = Character(name, 35, 100)
+        startGame()
     elif menu == '2':
         exit()
     else:
